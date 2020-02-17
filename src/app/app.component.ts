7c4c5e601883d607ab258a5a20f95d785d284513
HugoElcabezas <hugo.gomezllanos@cetys.edu.mx>
2020-02-17 10:32:40 -0800

    Actualización Correcta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+
+
 })
 export class AppComponent {
-  title = 'Trabajo';
+  miVariable = 'Hola';
+  // title = 'Trabajo';
+
+  obtenerValor(elemento: HTMLInputElement): void {
+    if (elemento.value === '') {
+      alert('No se puede introducir valor vacío');
+    } else {
+      console.log(elemento.value);
+      this.miVariable = elemento.value;
+    }
+  }
 }
